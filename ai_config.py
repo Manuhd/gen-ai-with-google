f4a796552178dcd7c06833299b82c14e725f14e3
Manuhd <41427184+Manuhd@users.noreply.github.com>
2025-10-16 21:41:17 +0530

    Update ai_config.py

--- ai_config.py
+++ ai_config.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 
 # Configure once with your API key
-genai.configure(api_key="AIzaSyBUw8WDMDk5dAnm3AqeBUEhDPbrZVE93ZE")
+genai.configure(api_key="API KEY")
 
 model = genai.GenerativeModel("gemini-2.5-flash")
+
